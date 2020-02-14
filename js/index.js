209232c51ebf84f5476e6ac88887e986f396cdf2
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-02-13 23:01:12 -0500

    add nav bar background and text/link color changes

--- js/index.js
+++ js/index.js
@@ -4,3 +4,14 @@ let navbar = document.querySelector('.nav-container');
 links.forEach(item => (item.addEventListener('click', e => {
     e.preventDefault();
 })))
+navbar.addEventListener("mouseover", e => {
+    navbar.style.backgroundColor = 'teal';
+    navbar.style.color = 'white';
+    links.forEach((item => item.style.color = 'white'))
+})
+navbar.addEventListener("mouseout", e => {
+    navbar.style.backgroundColor = 'white';
+    navbar.style.color = "#212529";
+    links.forEach((item => item.style.color = '#212529'))
+
+})
