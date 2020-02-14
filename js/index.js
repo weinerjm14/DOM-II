84667c2a6f2db64d6d352c6a6a6ca33d1c62050a
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-02-13 22:54:09 -0500

    add working prevent Default on nav links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+let links = document.querySelectorAll('a');
+let navbar = document.querySelector('.nav-container');
+links.forEach(item => (item.addEventListener('click', e => {
+    e.preventDefault();
+})))
