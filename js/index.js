eb761b38744e6f5108abe4e4ac6fe4e64a9ebcca
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-02-14 13:31:26 -0500

    add mouse event to header image

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 // Your code goes here
 let links = document.querySelectorAll('a');
 let navbar = document.querySelector('.nav-container');
+let buttons = document.querySelectorAll('.btn');
+let images = document.querySelectorAll('img');
 links.forEach(item => (item.addEventListener('click', e => {
     e.preventDefault();
 })))
@@ -14,4 +16,10 @@ navbar.addEventListener("mouseout", e => {
     navbar.style.color = "#212529";
     links.forEach((item => item.style.color = '#212529'))
 
+})
+images[0].addEventListener('mouseenter', (e)=> {
+    images[0].style.border = '2px inset green';
+})
+images[0].addEventListener('mouseout', (e)=> {
+    images[0].style.border = 'none';
 })
